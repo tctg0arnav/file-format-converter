b21ce994abfc5a32cb1b553e823c2a1066dff634
tctg0arnav <59003715+tctg0arnav@users.noreply.github.com>
2025-04-12 02:03:42 +0530

    .

--- formats_hub/__init__.py
+++ formats_hub/__init__.py
@@ -1,3 +1,5 @@
-__all__ = ["png"]
+__all__ = ["png", "jpg", "mp4"]
 
 from . import png
+from . import jpg
+from . import mp4
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ["test_jpg", "test_png"]
+__all__ = ["test_jpg", "test_png", "test_mp4"]
 
 from . import test_jpg
 from . import test_png
+from . import test_mp4
